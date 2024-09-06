073a67f5941f070c57dc944517e3a0f6f731cffe
NatsukiKoo <39581263+AlterGu@users.noreply.github.com>
2024-09-06 13:08:05 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo '检测hy2定时任务'
-(crontab -l | grep -F "screen") && echo "定时任务存在" || (crontab -l; echo "ps aux|grep 'screen'|grep -v 'grep' || cd /home/kagero/s-box && screen -S box ./web run" | crontab - && echo '设置定时任务成功')
+(crontab -l | grep -F "screen" && echo "定时任务存在") || (crontab -l; echo "ps aux|grep 'screen'|grep -v 'grep' || cd /home/kagero/s-box && screen -S box ./web run" | crontab - && echo '设置定时任务成功')
 
