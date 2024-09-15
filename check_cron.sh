f81edd7c17c0d730b757d1fc136d83834bfa6cfc
NatsukiKoo <39581263+AlterGu@users.noreply.github.com>
2024-09-15 12:08:17 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo '检测hy2定时任务'
-(crontab -l | grep -F "web run" && echo "定时任务存在") || (crontab -l; echo "bash run_hy2.sh " | crontab - && echo '设置定时任务成功')
+(crontab -l | grep -F "run_hy2" && echo "定时任务存在") || (crontab -l; echo "*/2 * * * * bash ~/s-box/run_hy2.sh" | crontab - && echo '设置定时任务成功')
 
