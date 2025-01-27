6a4477439cbcc23e8964b3babfce0255bcf00c1e
NatsukiKoo <39581263+AlterGu@users.noreply.github.com>
2025-01-27 22:17:46 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+uptime
 #echo '检测hy2定时任务'
 #(crontab -l | grep -F "run_hy2" && echo "定时任务存在") || (crontab -l; echo "*/2 * * * * bash ~/s-box/run_hy2.sh" | crontab - && echo '设置定时任务成功')
-uptime
+
+echo '检测alist定时任务'
+(crontab -l | grep -F "run_alist" && echo "定时任务存在") || (crontab -l; echo "*/2 * * * * bash ~/domains/alist.wakuu.us.kg/run_alist.sh" | crontab - && echo '设置定时任务成功')
